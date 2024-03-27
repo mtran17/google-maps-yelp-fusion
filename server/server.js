88d13fdf81580c36043c8a3599d52539e1976972
ryan-avancena <ravancena.dev@gmail.com>
2024-03-27 12:07:30 -0700

    added server

--- server/server.js
+++ server/server.js
@@ -1,33 +1,38 @@
 const express = require('express')
 const axios = require('axios')
 const app = express()
+require('dotenv').config();
 
-REACT_APP_YELP_APP_KEY = 'QRy3peTwKHhnlMYWXQNdvfhdDXDAWmQ68YnIH2MewwmMte30eelvDw34s2OR03QCZQ5HuPS0QdtEjHcZAazrR5DFePKeaG3hvaOakvnR9waOP0jgQgEoQ3B8WCi4ZXYx'
-
-async function searchBusiness(term, location) {
-    try{
-        const response = await axios.get('https://api.yelp.com/v3/businesses/search',{
-            headers : {
-                Authorization:`Bearer QRy3peTwKHhnlMYWXQNdvfhdDXDAWmQ68YnIH2MewwmMte30eelvDw34s2OR03QCZQ5HuPS0QdtEjHcZAazrR5DFePKeaG3hvaOakvnR9waOP0jgQgEoQ3B8WCi4ZXYx`
+async function searchBusiness(pLat, pLng, pTerm, pRadius, pPrice) {
+    try {
+        const response = await axios.get('https://api.yelp.com/v3/businesses/search', {
+            headers: {
+                Authorization: `Bearer ${process.env.REACT_APP_YELP_APP_KEY}`
             },
             params: {
-                term, 
-                location,
-                limit : 10
+                latitude : pLat, // Pass latitude
+                longitude :  pLng, // Pass longitude
+                term : pTerm,
+                radius : pRadius, 
+                price : pPrice,
+                limit: 3
             }
-        })
-        return response.data
+        });
+        return response.data;
     } catch (e) {
-        console.error('error: ', e)
+        console.error('error: ', e);
+        throw e; // Rethrow error to handle it in the calling function
     }
 }
 
 app.get("/api", async (req,res) => {
-    // res.json({"users" : ["user1" , "user2", "user3" ]})
-    const businesses = await searchBusiness('food', 'Newyork');
-    console.log(businesses)
+    const { lat, lng, term, radius, price } = req.query;
+    // console.log(lat, lng, term, radius, price)
+
+    const businesses = await searchBusiness(lat, lng, term, radius, price);
+    // console.log(businesses)
     res.json(businesses)
 })
 
-app.listen(5000, () => {console.log("Sever started on port 5000")})
+app.listen(5000, () => {console.log("Server started on port 5000")})
 
